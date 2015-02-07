cfcb4de6dad50798da3a5b27fc78a79bd0a2d109
Skytim <sky22357168@gmail.com>
2015-02-07 11:49:31 +0800

    Sign-up完成

--- js/sign-up.js
+++ js/sign-up.js
@@ -3,7 +3,7 @@ $(document).on("pageinit","#initialpage",function(){
 
   $.getScript( 'http://www.parsecdn.com/js/parse-1.3.0.min.js', function() {
   Parse.initialize("x40POIuJX8WKxgMEGKP1LAW6xmI3oU2g6cRpTjo9", "99gMOxg5JMzbneZ4tCoxrXIp1aLVnLtP7QJHRbKR");
-
+  
   $(document).on('click', '#sign-up-submit', function() { 
     var username=$('input[name="txt-user-name"]').val();
     var email=$('input[name="txt-email"]').val();
@@ -22,6 +22,8 @@ $(document).on("pageinit","#initialpage",function(){
     user.signUp(null, {
            success: function(user) {
              // Hooray! Let them use the app now.
+
+           $.mobile.changePage("sign-in.html", "slideup"); 
                                      },
            error: function(user, error) {
              // Show the error message somewhere and let the user try again.
